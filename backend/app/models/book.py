7488ced1bc3d46a52e271fa5b9a27989cb93b239
BinhLe15 <binhnam1505@gmail.com>
2025-04-16 09:46:13 +0700

    feat: remove bookauthor and bookcategory tables

--- backend/app/models/book.py
+++ backend/app/models/book.py
@@ -1,19 +1,6 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, List
 
-
-class BookCategory(SQLModel, table=True):
-    """Link model for Book and Category."""
-    
-    book_id: Optional[int] = Field(default=None, foreign_key="book.id", primary_key=True)
-    category_id: Optional[int] = Field(default=None, foreign_key="category.id", primary_key=True)
-
-class BookAuthor(SQLModel, table=True):
-    """Link model for Book and Author."""
-    
-    book_id: Optional[int] = Field(default=None, foreign_key="book.id", primary_key=True)
-    author_id: Optional[int] = Field(default=None, foreign_key="author.id", primary_key=True)
-
 class Book(SQLModel, table=True):
     """Book model for the application."""
     
