ea4e0fc2f1fe0c108f11e64f7dd77f0c76730932
BinhLe15 <binhnam1505@gmail.com>
2025-05-01 00:26:23 +0700

    feat: add sort by condition for review api

--- backend/app/api/routers/reviews.py
+++ backend/app/api/routers/reviews.py
@@ -18,7 +18,7 @@ async def create_review_by_book_id(review_create: ReviewCreate, book_id: int, se
     return review
 
 @router.get("/{book_id}")
-async def get_reviews_by_book_id(book_id: int, rating: int = 5, sort_by: str = "newest to oldest", skip: int = 0, limit: int = 10, session: Session = Depends(get_session)):
+async def get_reviews_by_book_id(book_id: int, rating: int = 0, sort_by: str = "newest to oldest", skip: int = 0, limit: int = 10, session: Session = Depends(get_session)):
     reviews = get_reviews(session, book_id, rating, sort_by, skip, limit)
     if not reviews:
         raise HTTPException(status_code=404, detail="No reviews found for this book")
