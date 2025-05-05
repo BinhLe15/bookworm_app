c3ba7171b1b8d4f3a29ddc55a58a98d12b4d3751
BinhLe15 <binhnam1505@gmail.com>
2025-05-05 22:46:11 +0700

    feat: add book icon image and not found page

---
+++ frontend/src/pages/NotFound.tsx
@@ -0,0 +1,12 @@
+// src/pages/NotFound.tsx
+import React from 'react'
+
+const NotFound: React.FC = () => {
+    return (
+        <div className='container mx-auto flex items-center justify-center h-screen'>
+            <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFound
